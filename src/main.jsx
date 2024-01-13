14ae1565e2c1bb633247daa42af3f8e51a6895b3
Auth <111557161+AuthFrl@users.noreply.github.com>
2024-01-13 20:45:28 +0530

    restart ahem

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-// main.tsx or main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {NextUIProvider} from '@nextui-org/react'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <App />
   </React.StrictMode>,
 )
