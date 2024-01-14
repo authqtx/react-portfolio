a45879e5f83606eb871f0612a63cf5588ee2be22
Auth <111557161+AuthFrl@users.noreply.github.com>
2024-01-14 22:25:11 +0530

    img + reponsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Name from './components/Name';
 import './index.css';
+import HuTao from './components/HuTao';
 
 function App() {
   return (
     <>
       <Name />
-     
+     <HuTao />
     </>
   );
 }
---
+++ src/components/HuTao.jsx
@@ -0,0 +1,9 @@
+import HuTao from '../img/HuTao.jpg';
+
+const Gradient = () => {
+    return(
+        <img src={HuTao} className='absolute md:right-0 top-0 w-96 h-128 mt-96 md:mt-16  rounded-md'/>
+    );
+}
+
+export default Gradient
