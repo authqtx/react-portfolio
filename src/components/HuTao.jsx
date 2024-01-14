21a0d787b5f45189bf756f1be0894325105c325e
Auth <111557161+AuthFrl@users.noreply.github.com>
2024-01-14 22:41:06 +0530

    animation on img

--- src/components/HuTao.jsx
+++ src/components/HuTao.jsx
@@ -1,9 +1,12 @@
 import HuTao from '../img/HuTao.jpg';
+import { motion } from "framer-motion"
 
 const Gradient = () => {
-    return(
-        <img src={HuTao} className='absolute md:right-0 top-0 w-96 h-128 mt-96 md:mt-16  rounded-md'/>
+    return (
+        <>
+            <motion.img whileHover={{ x: -200 }} className='absolute md:right-0 top-0 w-96 h-128 mt-96 md:mt-16 rounded-md' src={HuTao} />
+        </>
     );
 }
 
-export default Gradient
+export default Gradient;
