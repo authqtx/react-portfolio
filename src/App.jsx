0cbd77dca89bd54acd5b61528c984a42a4143c88
Auth <111557161+AuthFrl@users.noreply.github.com>
2024-01-19 21:52:04 +0530

    FRAMER MOTION HELL YEAH

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import Name from "./components/Name";
 import "./index.css";
 import Links from "./components/Links";
 import NavbarComponent from "./components/NavbarComponent";
+import ParallaxText from "./components/ParallaxText";
 
 function App() {
-  return (
+  return(
     <>
       <NavbarComponent />
       <Name />
       <Links />
+      <section>
+        {/* Using the ParallaxText component */}
+        <ParallaxText baseVelocity={-10}>REACTJS</ParallaxText>
+        <ParallaxText baseVelocity={2}>GASP FRAMER-MOTION REACT-SPRING</ParallaxText>
+      </section>
     </>
   );
 }
